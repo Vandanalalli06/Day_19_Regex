b7714bc729853f529ee49fc71fe3ad7ecc425c06
Vandanalalli06 <vandanalalli06@gmail.com>
2022-08-03 17:45:32 +0530

    Valid Mobile Number

--- Regex/Regex/Program.cs
+++ Regex/Regex/Program.cs
@@ -10,9 +10,9 @@ namespace RegexProblem
             Console.WriteLine("Welcome To User Registration Problems");
 
 
-            ValidEmail validEmail = new ValidEmail();
-            bool validTest1 = validEmail.validateEmail("abc.xyz@bl.co.in");
-            bool validTest2 = validEmail.validateEmail("abc.xyz@in");
+            ValidMobile validmobile = new ValidMobile();
+            bool validTest1 = validmobile.validateString("9156784");
+            bool validTest2 = validmobile.validateString("91 6875649781");
             Console.WriteLine("{0} {1} ", (validTest1) ? "Valid" : "Invalid", (validTest2) ? "Valid" : "Invalid");
             Console.ReadKey();
         }
