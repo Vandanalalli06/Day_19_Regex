bf2c7f039ba3f0cfff7df301b6ec682efe16a10d
Vandanalalli06 <vandanalalli06@gmail.com>
2022-08-03 09:20:41 +0530

    Welcome to regex Problems

---
+++ Regex/Regex/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RegexProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To User Registration Problems");
+
+           
+            ValidFirstName validFirstName = new ValidFirstName();
+            bool validTest1 = validFirstName.validateFirstName("Vandana");
+            bool validTest2 = validFirstName.validateFirstName("va");
+            Console.WriteLine("{0}  ", (validTest1) ? "Valid" : "Invalid");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
