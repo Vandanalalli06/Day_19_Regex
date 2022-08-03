14dfeb53d6367765af5778c93ddeb09e12abafc0
Vandanalalli06 <vandanalalli06@gmail.com>
2022-08-03 18:30:01 +0530

    Validate Password Rule atleast one Upper case

--- Regex/Regex/ValidPassword1.cs
+++ Regex/Regex/ValidPassword1.cs
@@ -9,7 +9,7 @@ namespace RegexProblem
 {
     class ValidPassword
     {
-        public static string Password1 = "^[a-z A-Z]{8,}$";
+        public static string Password1 = "([A-Z]{1}[A-Za-z]{7,})";
         public bool validatePassword(string Password)
         {
             return Regex.IsMatch(Password, Password1);
